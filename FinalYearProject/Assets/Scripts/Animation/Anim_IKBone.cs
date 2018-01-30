2974264383b54bc9d0203712736a1d092f4ec526
Unknown <henrybell96@hotmail.com>
2018-01-30 18:32:12 +0000

    Basic 2D IK

---
+++ FinalYearProject/Assets/Scripts/Animation/Anim_IKBone.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Anim_IKBone : MonoBehaviour {
+
+    public Vector2 pStartNode { get { return this.transform.position; } }
+    public Vector2 pBoneVector { get { return this.transform.up * Length; } }
+    public Vector2 pEndNode { get { return pStartNode + pBoneVector; } }
+
+    [SerializeField]
+    private float Length = 1f;
+
+
+    //-----------------------------------Unity Functions-----------------------------------
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.magenta;
+        Gizmos.DrawLine(pStartNode, pEndNode);
+    }
+
+
+    //-----------------------------------Public Functions----------------------------------
+
+    public void PositionStartNode(Vector2 pos)
+    {
+        var currentEndNode = pEndNode;
+        this.transform.position = pos;
+        LookAt(currentEndNode);
+    }
+
+    public void PositionEndNode(Vector2 pos)
+    {
+        LookAt(pos);
+        transform.Translate(Vector3.up * ((pos - pStartNode).magnitude - Length));
+    }
+
+
+    //-----------------------------------Private Functions----------------------------------
+
+    private void LookAt(Vector2 pos)
+    {
+        Vector3 diff3D = (new Vector3(pos.x, pos.y, 0f) - transform.position).normalized;
+        float zRot = Mathf.Atan2(diff3D.y, diff3D.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(0f, 0f, zRot - 90f);
+    }
+}
---
+++ FinalYearProject/Assets/Scripts/Animation/Anim_IKRoot.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[SelectionBase]
+public class Anim_IKRoot : MonoBehaviour {
+
+    [SerializeField]
+    private List<Anim_IKBone> mBones = new List<Anim_IKBone>();
+
+    private Vector2 pPos2D { get { return new Vector2(transform.position.x, transform.position.y); } }
+
+    // TESTING
+    Vector3 mousePos;
+
+
+    //-----------------------------------Unity Functions-----------------------------------
+
+    private void Update()
+    {
+        mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        var targetPos = mousePos;
+
+        for (int i = mBones.Count - 1; i >= 0; i--)
+        {
+            mBones[i].PositionEndNode(targetPos);
+            targetPos = mBones[i].pStartNode;
+        }
+
+        for (int i = 0; i < mBones.Count; i++)
+        {
+            var parentEndPos = (i == 0) ? pPos2D : mBones[i - 1].pEndNode;
+            mBones[i].PositionStartNode(parentEndPos);
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.magenta;
+        Gizmos.DrawSphere(mousePos, 0.25f);
+    }
+}
