9cffb14ddbbcf039531c837a3acd3d3aa1ab4be0
Unknown <henrybell96@hotmail.com>
2018-04-25 19:35:21 +0100

    Created Main Menu & Anim Demo Scene

---
+++ FinalYearProject/Assets/Scripts/Communication/Comm_UIToAnim.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Comm_UIToAnim : MonoBehaviour {
+
+    [SerializeField]
+    private Anim_AnimationController AnimController;
+    [SerializeField]
+    private Slider WalkSpeedSlider;
+    [SerializeField]
+    private Slider StrideSlider;
+    [SerializeField]
+    private Slider StepHeightSlider;
+    [SerializeField]
+    private Slider BounceHeightSlider;
+    [SerializeField]
+    private Slider ArmSwingSlider;
+    [SerializeField]
+    private Slider ArmBendSlider;
+    [SerializeField]
+    private Slider LeanSlider;
+    [SerializeField]
+    private Slider HunchSlider;
+    [SerializeField]
+    private Slider HeadTiltSlider;
+
+
+    //-----------------------------------Unity Functions-----------------------------------
+
+    private void Start()
+    {
+        WalkSpeedSlider.value = AnimController.pWalkSpeed;
+        StrideSlider.value = AnimController.pStride;
+        StepHeightSlider.value = AnimController.pStepHeight;
+        BounceHeightSlider.value = AnimController.pBounceHeight;
+        ArmSwingSlider.value = AnimController.pArmSwing;
+        ArmBendSlider.value = AnimController.pArmBend;
+        LeanSlider.value = AnimController.pLean;
+        HunchSlider.value = AnimController.pHunch;
+        HeadTiltSlider.value = AnimController.pHeadTilt;
+    }
+
+    private void Update()
+    {
+        AnimController.pWalkSpeed = WalkSpeedSlider.value;
+        AnimController.pStride = StrideSlider.value;
+        AnimController.pStepHeight = StepHeightSlider.value;
+        AnimController.pBounceHeight = BounceHeightSlider.value;
+        AnimController.pArmSwing = ArmSwingSlider.value;
+        AnimController.pArmBend = ArmBendSlider.value;
+        AnimController.pLean = LeanSlider.value;
+        AnimController.pHunch = HunchSlider.value;
+        AnimController.pHeadTilt = HeadTiltSlider.value;
+    }
+}
---
+++ FinalYearProject/Assets/Scripts/UI/UI_UIManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UI_UIManager : MonoBehaviour {
+
+    public static UI_UIManager Instance;
+
+
+    //-----------------------------------Unity Functions-----------------------------------
+
+    private void Awake()
+    {
+        if (Instance == null)
+            Instance = this;
+        else
+            Destroy(this);
+    }
+
+
+    //-----------------------------------Public Functions----------------------------------
+
+    public void LoadScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+
+    public void QuitApplication()
+    {
+        Application.Quit();
+    }
+
+
+    //----------------------------------Private Functions----------------------------------
+   
+}
